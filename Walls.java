4fd2886b357c369f626c87ed529825c161580c86
GnomeHooligan <ellordicus@yahoo.com>
2018-03-23 20:00:48 -0700

    Add files via upload

---
+++ Walls.java
@@ -0,0 +1,13 @@
+
+package agameofmyown;
+
+public abstract class Walls extends Entities
+{
+    public Walls()
+    {
+        this.bxSpeed = 0;
+        this.bySpeed = 0;
+        this.B_WIDTH = 32;
+        this.B_HEIGHT = 32;
+    }
+}
